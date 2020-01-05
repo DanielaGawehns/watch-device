6953dc1510267f9e546e7e09233bed032c7add7c
peterbjornx <me@pbx.sh>
2020-01-05 19:47:36 +0100

    Make network layer compile under Linux

--- src/network.c
+++ src/network.c
@@ -8,18 +8,9 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <errno.h>
-#include <dlog.h>
 #include <fcntl.h>
 #include <unistd.h>
-
-#ifdef  LOG_TAG
-#undef  LOG_TAG
-#endif
-#define LOG_TAG "sensorbasicui"
-
-#if !defined(PACKAGE)
-#define PACKAGE "org.example.sensorbasicui"
-#endif
+#include "network.h"
 
 int                udp_sock;
 int                ulst_sock;
@@ -49,7 +40,7 @@ void ulst_init() {
 	ulst_sock = socket( AF_INET, SOCK_DGRAM, 0 );
 
 	if ( ulst_sock < 0 ) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to open UDP socket: %s (%i)",
+		net_log_err( "Failed to open UDP socket: %s (%i)",
 				strerror(errno), errno);
 		return;
 	}
@@ -57,7 +48,7 @@ void ulst_init() {
 	r = fcntl( ulst_sock, F_SETFL, 0 );
 
 	if ( r < 0 ) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to blocking mode: %s (%i)",
+		net_log_err( "Failed to blocking mode: %s (%i)",
 				strerror(errno), errno);
 		close( ulst_sock );
 		return;
@@ -72,7 +63,7 @@ void ulst_init() {
 	r = setsockopt(ulst_sock, SOL_SOCKET, SO_BROADCAST, &bc_enable, sizeof bc_enable);
 
 	if ( r < 0 ) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to enable broadcast: %s (%i)",
+		net_log_err( "Failed to enable broadcast: %s (%i)",
 				strerror(errno), errno);
 		close( ulst_sock );
 		return;
@@ -81,16 +72,16 @@ void ulst_init() {
 	r = setsockopt( ulst_sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
 
 	if ( r < 0 ) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to set timeout: %s (%i)",
+		net_log_err( "Failed to set timeout: %s (%i)",
 				strerror(errno), errno);
 		close( ulst_sock );
 		return;
 	}
 
-	r = bind( ulst_sock, &any_addr, sizeof any_addr );
+	r = bind( ulst_sock, (struct sockaddr *) &any_addr, sizeof any_addr );
 
 	if ( r < 0 ) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to bind UDP socket: %s (%i)",
+		net_log_err( "Failed to bind UDP socket: %s (%i)",
 					strerror(errno), errno);
 		return;
 	}
@@ -105,7 +96,7 @@ void broadcast_init() {
 	udp_sock = socket( AF_INET, SOCK_DGRAM, 0 );
 
 	if ( udp_sock < 0 ) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to open UDP socket: %s (%i)",
+		net_log_err( "Failed to open UDP socket: %s (%i)",
 				strerror(errno), errno);
 		return;
 	}
@@ -116,7 +107,7 @@ void broadcast_init() {
 	r = setsockopt(udp_sock, SOL_SOCKET, SO_BROADCAST, &bc_enable, sizeof bc_enable);
 
 	if ( r < 0 ) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to enable broadcast: %s (%i)",
+		net_log_err( "Failed to enable broadcast: %s (%i)",
 				strerror(errno), errno);
 		close( udp_sock );
 		return;
@@ -128,14 +119,15 @@ void broadcast_send() {
 
 	const char *mess = "HelloWorld!"; //TODO: What do we send here
 
-    if(sendto( udp_sock, mess, strlen(mess), 0, &bcast_addr, sizeof bcast_addr ) < 0) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to send broadcast: %s (%i)",
+    if(sendto( udp_sock, mess, strlen(mess), 0, 
+	           (struct sockaddr *) &bcast_addr, sizeof bcast_addr ) < 0) {
+		net_log_err( "Failed to send broadcast: %s (%i)",
 				strerror(errno), errno);
 		close( udp_sock );
 		return;
 	}
 
-	dlog_print(DLOG_INFO, LOG_TAG, "Sent broadcast");
+	net_log_info( "Sent broadcast");
 
 }
 
@@ -145,20 +137,22 @@ int broadcast_recv() {
 	socklen_t len = sizeof srv_addr;
 	for ( ;; ) {
 		/* Receive a single datagram from the server */
-		rsz = recvfrom( ulst_sock, buf, sizeof buf - 1, 0, &srv_addr, &len );
+		rsz = recvfrom( ulst_sock,
+						buf, sizeof buf - 1, 
+						0, (struct sockaddr *) &srv_addr, &len );
 		if ( rsz < 0 ) {
-			dlog_print(DLOG_ERROR, LOG_TAG, "Failed to receive server address: %s (%i)",
+			net_log_err( "Failed to receive server address: %s (%i)",
 					strerror(errno), errno);
 			return -1;
 		}
 		buf[rsz] = 0;
 		if ( rsz == 0 || 0 != strncmp( buf, srv_ident, sizeof buf - 1 ) ) {
-			dlog_print(DLOG_WARN, LOG_TAG, "Received invalid ack: %s", buf);
+			net_log_warn( "Received invalid ack: %s", buf);
 			continue;
 		}
 		break;
 	}
-	dlog_print(DLOG_INFO, LOG_TAG, "Acked by server at %s", inet_ntoa(srv_addr.sin_addr));
+	net_log_info( "Acked by server at %s", inet_ntoa(srv_addr.sin_addr));
     srv_addr.sin_port = (in_port_t)htons(2114);
 	return 0;
 }
@@ -186,7 +180,7 @@ int client_connect() {
 	clnt_sock = socket( AF_INET, SOCK_STREAM, 0 );
 
 	if ( clnt_sock < 0 ) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to open TCP socket: %s (%i)",
+		net_log_err( "Failed to open TCP socket: %s (%i)",
 				strerror(errno), errno);
 		return -1;
 	}
@@ -194,7 +188,7 @@ int client_connect() {
 	r = fcntl( clnt_sock, F_SETFL, 0 );
 
 	if ( r < 0 ) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to blocking mode: %s (%i)",
+		net_log_err( "Failed to blocking mode: %s (%i)",
 				strerror(errno), errno);
 		close( clnt_sock );
 		return -1;
@@ -206,16 +200,17 @@ int client_connect() {
 	r = setsockopt( clnt_sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
 
 	if ( r < 0 ) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to set timeout: %s (%i)",
+		net_log_err( "Failed to set timeout: %s (%i)",
 				strerror(errno), errno);
 		close( clnt_sock );
 		return -1;
 	}
 
-	r = connect( clnt_sock, &srv_addr, sizeof srv_addr );
+	r = connect( clnt_sock, 
+	             (struct sockaddr *) &srv_addr, sizeof srv_addr );
 
 	if ( r < 0 ) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to connect TCP socket: %s (%i)",
+		net_log_err( "Failed to connect TCP socket: %s (%i)",
 					strerror(errno), errno);
 		return -1;
 	}
@@ -234,7 +229,7 @@ int client_write( const void *data, size_t size ) {
 	for ( off = 0; off < size; off += ws ) {
 		ws = send( clnt_sock, data + off, size - off, 0 );
 		if ( ws < 0 ) {
-			dlog_print(DLOG_ERROR, LOG_TAG, "Failed to send data over TCP: %s (%i)",
+			net_log_err( "Failed to send data over TCP: %s (%i)",
 					strerror(errno), errno);
 			close( clnt_sock );
 			return -1;
@@ -261,7 +256,7 @@ int client_read( void *data, size_t size ) {
 	for ( off = 0; off < size; off += ws ) {
 		ws = recv( clnt_sock, data + off, size - off, 0 );
 		if ( ws < 0 ) {
-			dlog_print(DLOG_ERROR, LOG_TAG, "Failed to receive data over TCP: %s (%i)",
+			net_log_err( "Failed to receive data over TCP: %s (%i)",
 					strerror(errno), errno);
 			close( clnt_sock );
 			return -1;
--- src/network.h
+++ src/network.h
@@ -16,7 +16,7 @@ void broadcast_init( );
 void broadcast_send( );
 int broadcast_recv( );
 void broadcast_stop( );
-void broadcast_hello( );
+int broadcast_hello( );
 int client_connect( );
 void client_close( );
 int client_write( const void *data, size_t size );
@@ -34,5 +34,8 @@ int client_available( );
  */
 int client_read( void *data, size_t size );
 
+void net_log_err( const char *fmt, ... );
+void net_log_warn( const char *fmt, ... );
+void net_log_info( const char *fmt, ... );
 
 #endif /* NETWORK_H_ */
